58ac4b29ae72aa590a78dcad288fe785c80f891d
Ivan Lazaric <ivan.lazaric1@gmail.com>
2025-11-18 01:09:59 +0100

    expand `->` test

--- clang/ivl_test_8.cpp
+++ clang/ivl_test_8.cpp
@@ -5,12 +5,19 @@ struct S {};
 [[ivl::ufcs]]
 void fn_ref(const S&);
 
-[[ivl::ufcs]]
-void fn_ptr(const S*);
+// [[ivl::ufcs]]
+// void fn_ptr(const S*);
+
+struct T { S* operator->(); };
+
+struct U { T operator->(); };
 
 void use() {
   S s;
   s.fn_ref(); // should already work
   (&s)->fn_ref(); // no idea, doesnt work, want it to work
   // (&s).fn_ptr(); // no idea, doesnt work, unsure if want to work
+
+  U u;
+  u->fn_ref();
 }
